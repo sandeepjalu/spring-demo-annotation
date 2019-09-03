50e61630824ac340e93ccd7a5f09284431b9fab4
Sandeep Kumar Jaluthariya <sandeep.jaluthariya@walmartlabs.com>
2019-09-03 20:48:07 +0530

    setter method

--- src/com/learn/demo/TennisCoach.java
+++ src/com/learn/demo/TennisCoach.java
@@ -11,12 +11,23 @@ import com.learn.demo.interfaces.FortuneService;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	FortuneService fortuneService;
 	
+	public TennisCoach() {
+		System.out.println(">>> default constractor TennisCoach");
+	}
+	/*
 	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">>> inside setFortuneService() method");
+		this.fortuneService = fortuneService;
+	}*/
+	
+	/*@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
-	}
+	}*/
 	
 	@Override
 	public String getDailyWorkout() {
